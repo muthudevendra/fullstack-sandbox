519056498b8152162712c511b38dd85990fba5b0
Muthu Devendra <muthupriyadarshani@gmail.com>
2021-11-24 14:53:25 +0530

    Modify fetch and view list state management

--- frontend/src/todos/components/ToDoLists.jsx
+++ frontend/src/todos/components/ToDoLists.jsx
@@ -13,21 +13,20 @@ import debounce from 'lodash.debounce';
 
 export const ToDoLists = ({ style }) => {
   const [toDoLists, setToDoLists] = useState({
-    '0000000001': {
+    1: {
       id: 1,
       title: 'First List',
       todos: []
     },
-    '0000000002': {
+    2: {
       id: 2,
       title: 'Second List',
       todos: []
     }
   });
 
-  
+
   const [activeList, setActiveList] = useState();
-  const [activeListId, setActiveListId] = useState();
 
   useEffect(() => {
     getPersonalTodos();
@@ -37,26 +36,14 @@ export const ToDoLists = ({ style }) => {
     const data = await client("/todos", "GET");
     const listOne = data.filter((item) => item.listId === 1);
     const listTwo = data.filter((item) => item.listId === 2);
-    const modifiedData = {
-      '0000000001': {
-        id: 1,
-        title: 'First List',
-        todos: listOne
-      },
-      '0000000002': {
-        id: 2,
-        title: 'Second List',
-        todos: listTwo
-      }
-    };
-    setToDoLists(modifiedData);
+    setToDoLists({ 1: { ...toDoLists[1], todos: listOne }, 2: { ...toDoLists[2], todos: listTwo } });
   }
 
   const createItem = async () => {
     await client(`/todos`, "POST", {
       body: {
         text: "",
-        listId: activeListId,
+        listId: activeList,
         isComplete: false,
       }
     });
@@ -69,26 +56,26 @@ export const ToDoLists = ({ style }) => {
   };
 
   const updateItem = useCallback(
-		debounce(async (id, value) => {
+    debounce(async (id, value) => {
       await client(`/todo/${id}`, "PUT", {
         body: {
           text: value
         }
       });
     }, 1000),
-		[],
-	);
+    [],
+  );
 
   const changeStatus = useCallback(
-		debounce(async (id, value) => {
+    debounce(async (id, value) => {
       await client(`/todo/${id}`, "PUT", {
         body: {
           isComplete: value
         }
       });
     }, 500),
-		[],
-	);
+    [],
+  );
 
   return <Fragment>
     <Card style={style}>
@@ -105,7 +92,6 @@ export const ToDoLists = ({ style }) => {
             onClick={() => {
               getPersonalTodos();
               setActiveList(key);
-              setActiveListId(toDoLists[key].id)
             }}
           >
             <ListItemIcon>
